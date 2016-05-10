005135b5cb52b08e3c6d8bb77f5c8c8477e1bd39
cybermarinella <marinella.mastrosimone@gmail.com>
2016-05-10 17:57:03 +0200

    Random Colors

--- js/script.js
+++ js/script.js
@@ -1,17 +1,53 @@
+   var $mycolor = getRandomColor();
 
-      var altezza = $( window ).height();
-      $( ".container>.row>div" ).height(altezza);
+  $('#refresh').click(function() {
+      location.reload();
+  });
+/*
+* Replace all SVG images with inline SVG
+*/
 
-      $('#pittogramma pre').fadeTo( "slow", 0.33);
-      $('#pittogramma .img01').fadeTo( "0.1", 1 );
+$('img.svg').each(function(){
+  var $img = $(this);
+  var imgID = $img.attr('id');
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
 
-      $('#structure pre').fadeTo( "slow", 0.33);
-      $('#structure .img01').fadeTo( "0.1", 1 );
+    $.get(imgURL, function(data) {
+      // Get the SVG tag, ignore the rest
+      var $svg = $(data).find('svg');
 
-      $('#logotipo pre').fadeTo( "slow", 0.33);
-      $('#logotipo .img01').fadeTo( "0.1", 1 );
+      // Add replaced image's ID to the new SVG
+      if(typeof imgID !== 'undefined') {
+        $svg = $svg.attr('id', imgID);
+      }
+      // Add replaced image's classes to the new SVG
+      if(typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass+' replaced-svg');
+        $(".scuro").css({ fill: $mycolor });
+        $(".scuroStroke").css({ stroke: $mycolor });
+        $('.img01').fadeTo( "slow", 1 );
+      }
+                              
+      // Remove any invalid XML tags as per http://validator.w3.org
+      $svg = $svg.removeAttr('xmlns:a');
+                              
+      // Replace image with new SVG
+      $img.replaceWith($svg);
+    });
+
+  });
 
 
+
+   $(".navbar-inverse").css("background-color", $mycolor);
+   $("h2, h3, p, pre, body, nav a:hover, .interactivities a").css("color", $mycolor);
+       
+      //var altezza = $( window ).height();
+
+      //$( ".container>.row>div" ).height(altezza);
+
+      $('pre').fadeTo( "slow", 0.33);
 
       $( "#pittogramma .action00 h3" ).mouseover(function() {
         event.stopImmediatePropagation();
@@ -21,7 +57,7 @@
       });
       $( "#pittogramma .action00 h3" ).mouseout(function() {
         event.stopImmediatePropagation();
-        $('#pittogramma pre').fadeTo( "slow", 0.33 );
+        $('#pittogramma pre').fadeTo( "slow", 1 );
       });
 
       $( "#pittogramma .action01 h3" ).mouseover(function() {
@@ -32,21 +68,19 @@
       });
       $( "#pittogramma .action01 h3" ).mouseout(function() {
         event.stopImmediatePropagation();
-        $('#pittogramma pre').fadeTo( "slow", 0.33 );
+        $('#pittogramma pre').fadeTo( "slow", 0);
       });
 
       $( "#pittogramma .action02 h3" ).mouseover(function() {
         event.stopImmediatePropagation();
+        $('#pittogramma .img02').fadeTo( "0.1", 1 );
         $('#pittogramma pre').fadeTo( "slow", 0 );
          $('#pittogramma .img01').fadeTo( "slow", 0 );
-         $('#pittogramma .img02').fadeTo( "0.1", 1 );
       });
       $( "#pittogramma .action02 h3" ).mouseout(function() {
         event.stopImmediatePropagation();
         $('#pittogramma pre').fadeTo( "slow", 0.33 );
       });
-
-
 
 
       $( "#structure .action00 h3" ).mouseover(function() {
@@ -95,7 +129,7 @@
       });
       $( "#logotipo .action00 h3" ).mouseout(function() {
         event.stopImmediatePropagation();
-        $('#logotipo pre').fadeTo( "slow", 0.33 );
+        $('#logotipo pre').fadeTo( "slow", 0 );
       });
 
       $( "#logotipo .action01 h3" ).mouseover(function() {
@@ -107,7 +141,7 @@
       });
       $( "#logotipo .action01 h3" ).mouseout(function() {
         event.stopImmediatePropagation();
-        $('#logotipo pre').fadeTo( "slow", 0.33 );
+        $('#logotipo pre').fadeTo( "slow", 0 );
       });
 
       $( "#logotipo .action02 h3" ).mouseover(function() {
@@ -115,11 +149,11 @@
         $('#logotipo pre').fadeTo( "slow", 0 );
          $('#logotipo .img01').fadeTo( "slow", 0 );
          $('#logotipo .img02').fadeTo( "0.1", 0);
-         $('#logotipo .img03').fadeTo( "slow", 3 );
+         $('#logotipo .img03').fadeTo( "slow", 1 );
       });
       $( "#structure .action02 h3" ).mouseout(function() {
         event.stopImmediatePropagation();
-        $('#logotipo pre').fadeTo( "slow", 0.33 );
+        $('#logotipo pre').fadeTo( "slow", 0 );
       });
 
       //   dx links
@@ -143,41 +177,12 @@
           $('html, body').animate({scrollTop: $("#dont").offset().top-60}, 1000);
       });
 
-/*
-     * Replace all SVG images with inline SVG
-     */
-/*$(function(){
-    jQuery('img.svg').each(function(){
-        var $img = jQuery(this);
-        var imgID = $img.attr('id');
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
-    
-        jQuery.get(imgURL, function(data) {
-            // Get the SVG tag, ignore the rest
-            var $svg = jQuery(data).find('svg');
-    
-            // Add replaced image's ID to the new SVG
-            if(typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
-            }
-            // Add replaced image's classes to the new SVG
-            if(typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass+' replaced-svg');
-            }
-    
-            // Remove any invalid XML tags as per http://validator.w3.org
-            $svg = $svg.removeAttr('xmlns:a');
-            
-            // Check if the viewport is set, else we gonna set it if we can.
-            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-            }
-    
-            // Replace image with new SVG
-            $img.replaceWith($svg);
-    
-        }, 'xml');
-    
-    });
-});*/
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
