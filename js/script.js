a24678a2b843afc1d4bcef231776e00a34fe75c4
cybermarinella <marinella.mastrosimone@gmail.com>
2016-05-11 15:38:51 +0200

    fix bugs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
   var $mycolor =  "hsl("+ Math.floor((Math.random() * 360) + 1)+", 80%, 40%)";
   var $positivo;
-  var altezza = $( window ).height();
-
+  var $altezza = $( window ).height();
+  var $altezzaSvg  = $( '.svg' ).height();
+  var $altezzaObjectBox = $altezza-260;
+  
 
 /* ================= Change Interface depending on sun position (now Day Hours)  ===============*/  
   var d = new Date();
   var n = d.getHours();
-  
+
   if(n >=9 && n<=18 ){
     $positivo= true;
   }else{
@@ -14,11 +16,10 @@
   }
 
   /* ================= SETUP ===============*/
-  posneg();
-
-  $( ".objectBox" ).height(altezza-230);
-  $( ".objectBox >div" ).css("padding-top", altezza/7);    
+  $( ".objectBox" ).height($altezzaObjectBox);
+  $( ".objectBox" ).css("padding-top", $altezzaObjectBox/2-$altezzaSvg/2-60);   
   $('pre').fadeTo( "slow", 0.33);
+  posneg();
 
   /* ================= Replace all SVG images with inline SVG ===============*/
 
@@ -52,151 +53,147 @@
     });
   });
 
+  /* === scrolling page ====*/
+
+      $(".linkPittogramma").click(function(event) {
+        event.stopImmediatePropagation();
+          $('html, body').animate({ scrollTop: $("#pittogramma").offset().top-60}, 1000);
+      });
+      $(".linkStruttura").click(function() {
+        event.stopImmediatePropagation(event);
+          $('html, body').animate({scrollTop: $("#structure").offset().top-60}, 1000);
+      });
+      $(".linkSpace").click(function() {
+        event.stopImmediatePropagation(event);
+          $('html, body').animate({scrollTop: $("#space").offset().top-60}, 1000);
+      });
+      $(".linkLogotipo").click(function(event) {
+        event.stopImmediatePropagation();
+          $('html, body').animate({scrollTop: $("#logotipo").offset().top-60}, 1000);
+      });
+      $(".linkRandom").click(function(event) {
+        event.stopImmediatePropagation();
+          $('html, body').animate({scrollTop: $("#random").offset().top-60}, 1000);
+      });
+      $(".linkDont").click(function(event) {
+        event.stopImmediatePropagation();
+          $('html, body').animate({scrollTop: $("#dont").offset().top-60}, 1000);
+      });
+
 
  /* ================= INTERACTION ===============*/
-      $( "#pittogramma .action00 h3" ).mouseover(function() {
+      $( "#pittogramma .action00 h3" ).mouseover(function(event) {
         event.stopImmediatePropagation();
         $('#pittogramma pre').fadeTo( "fast", 1 );
          $('#pittogramma .img01').fadeTo( "slow", 0 );
          $('#pittogramma .img02').fadeTo( "slow", 0 );
       });
-      $( "#pittogramma .action00 h3" ).mouseout(function() {
+      $( "#pittogramma .action00 h3" ).mouseout(function(event) {
         event.stopImmediatePropagation();
         $('#pittogramma pre').fadeTo( "fast", 1 );
       });
 
-      $( "#pittogramma .action01 h3" ).mouseover(function() {
+      $( "#pittogramma .action01 h3" ).mouseover(function(event) {
         event.stopImmediatePropagation();
         $('#pittogramma pre').fadeTo( "fast", 0 );
         $('#pittogramma .img01').fadeTo( "fast", 1 );
         $('#pittogramma .img02').fadeTo( "fast", 0 );
       });
-      $( "#pittogramma .action01 h3" ).mouseout(function() {
+      $( "#pittogramma .action01 h3" ).mouseout(function(event) {
         event.stopImmediatePropagation();
         $('#pittogramma pre').fadeTo( "fast", 0);
       });
 
-      $( "#pittogramma .action02 h3" ).mouseover(function() {
+      $( "#pittogramma .action02 h3" ).mouseover(function(event) {
         event.stopImmediatePropagation();
         $('#pittogramma .img02').fadeTo( "fast", 1 );
         $('#pittogramma pre').fadeTo( "fast", 0 );
          $('#pittogramma .img01').fadeTo( "fast", 0 );
       });
-      $( "#pittogramma .action02 h3" ).mouseout(function() {
+      $( "#pittogramma .action02 h3" ).mouseout(function(event) {
         event.stopImmediatePropagation();
         $('#pittogramma pre').fadeTo( "fast", 0 );
       });
 
 
-      $( "#structure .action00 h3" ).mouseover(function() {
+      $( "#structure .action00 h3" ).mouseover(function(event) {
         event.stopImmediatePropagation();
         $('#structure pre').fadeTo( "slow", 0 );
         $('#structure .img01').fadeTo( "slow", 1 );
         $('#structure .img02').fadeTo( "0.1", 0 );
         $('#structure .img03').fadeTo( "0.1", 0 );
       });
-      $( "#structure .action00 h3" ).mouseout(function() {
+      $( "#structure .action00 h3" ).mouseout(function(event) {
         event.stopImmediatePropagation();
         $('#structure pre').fadeTo( "slow", 0 );
       });
 
-      $( "#structure .action01 h3" ).mouseover(function() {
+      $( "#structure .action01 h3" ).mouseover(function(event) {
         event.stopImmediatePropagation();
         $('#structure pre').fadeTo( "slow", 0 );
         $('#structure .img01').fadeTo( "slow", 0 );
         $('#structure .img02').fadeTo( "0.1", 1 );
         $('#structure .img03').fadeTo( "0.1", 0 );
       });
-      $( "#structure .action01 h3" ).mouseout(function() {
+      $( "#structure .action01 h3" ).mouseout(function(event) {
         event.stopImmediatePropagation();
         $('#structure pre').fadeTo( "slow", 0 );
       });
 
-      $( "#structure .action02 h3" ).mouseover(function() {
+      $( "#structure .action02 h3" ).mouseover(function(event) {
         event.stopImmediatePropagation();
         $('#structure pre').fadeTo( "slow", 0 );
          $('#structure .img01').fadeTo( "slow", 0 );
          $('#structure .img02').fadeTo( "0.1", 0);
          $('#structure .img03').fadeTo( "slow", 3 );
       });
-      $( "#structure .action02 h3" ).mouseout(function() {
+      $( "#structure .action02 h3" ).mouseout(function(event) {
         event.stopImmediatePropagation();
         $('#structure pre').fadeTo( "slow", 0 );
       });
 
 
-      $( "#logotipo .action00 h3" ).mouseover(function() {
+      $( "#logotipo .action00 h3" ).mouseover(function(event) {
         event.stopImmediatePropagation();
         $('#logotipo pre').fadeTo( "slow", 0 );
         $('#logotipo .img01').fadeTo( "slow", 1 );
         $('#logotipo .img02').fadeTo( "0.1", 0 );
         $('#logotipo .img03').fadeTo( "0.1", 0 );
       });
-      $( "#logotipo .action00 h3" ).mouseout(function() {
+      $( "#logotipo .action00 h3" ).mouseout(function(event) {
         event.stopImmediatePropagation();
         $('#logotipo pre').fadeTo( "slow", 0 );
       });
 
-      $( "#logotipo .action01 h3" ).mouseover(function() {
+      $( "#logotipo .action01 h3" ).mouseover(function(event) {
         event.stopImmediatePropagation();
         $('#logotipo pre').fadeTo( "slow", 0 );
         $('#logotipo .img01').fadeTo( "slow", 0 );
         $('#logotipo .img02').fadeTo( "0.1", 1 );
         $('#logotipo .img03').fadeTo( "0.1", 0 );
       });
-      $( "#logotipo .action01 h3" ).mouseout(function() {
+      $( "#logotipo .action01 h3" ).mouseout(function(event) {
         event.stopImmediatePropagation();
         $('#logotipo pre').fadeTo( "slow", 0 );
       });
 
-      $( "#logotipo .action02 h3" ).mouseover(function() {
+      $( "#logotipo .action02 h3" ).mouseover(function(event) {
         event.stopImmediatePropagation();
         $('#logotipo pre').fadeTo( "slow", 0 );
          $('#logotipo .img01').fadeTo( "slow", 0 );
          $('#logotipo .img02').fadeTo( "0.1", 0);
          $('#logotipo .img03').fadeTo( "slow", 1 );
       });
-      $( "#structure .action02 h3" ).mouseout(function() {
+      $( "#structure .action02 h3" ).mouseout(function(event) {
         event.stopImmediatePropagation();
         $('#logotipo pre').fadeTo( "slow", 0 );
       });
 
-      //   dx links
-
-      $(".linkPittogramma").click(function() {
-          $('html, body').animate({ scrollTop: $("#pittogramma").offset().top-60}, 1000);
-      });
-      $(".linkStruttura").click(function() {
-          $('html, body').animate({scrollTop: $("#structure").offset().top-60}, 1000);
-      });
-      $(".linkSpace").click(function() {
-          $('html, body').animate({scrollTop: $("#space").offset().top-60}, 1000);
-      });
-      $(".linkLogotipo").click(function() {
-          $('html, body').animate({scrollTop: $("#logotipo").offset().top-60}, 1000);
-      });
-      $(".linkRandom").click(function() {
-          $('html, body').animate({scrollTop: $("#random").offset().top-60}, 1000);
-      });
-      $(".linkDont").click(function() {
-          $('html, body').animate({scrollTop: $("#dont").offset().top-60}, 1000);
-      });
-
-
   /* ================= POSITIVO vs NEGATIVO ===============*/
 
-  $('#positivo').click(function() {
+  $('#posNeg').click(function() {
     $positivo=!$positivo;
     posneg();
-    $(this).fadeOut();
-    $('#negativo').fadeIn();
-  });
-
-   $('#negativo').click(function() {
-    $positivo=!$positivo;
-    posneg();
-    $(this).fadeOut();
-    $('#positivo').fadeIn();
   });
 
   $('.main').click(function() {
@@ -209,13 +206,13 @@ function posneg(){
   if($positivo){
     $('body, .interactivities a').css( "background-color", "#fff");
     $('nav').css( "background-color", $mycolor);
-    $("h2, h3, p, pre, body, nav a:hover, .interactivities a, .main a, .interactivities a").css("color", $mycolor);
+    $("h2, h3, p, pre, body, .interactivities a, .main a, .interactivities a").css("color", $mycolor);
     $(".scuro").css({ fill: $mycolor });
     $(".scuroStroke").css({ stroke: $mycolor });
   }else{
     $('body, .interactivities a, nav').css( "background-color", $mycolor);
     $('nav').css( "background-color", $mycolor);
-    $("h2, h3, p, pre, body, nav a:hover, .main a, .interactivities a, nav").css("color", "#fff");
+    $("h2, h3, p, pre, body, .main a, .interactivities a, nav").css("color", "#fff");
     $(".scuro").css({ fill: "#fff" });
     $(".scuroStroke").css({ stroke: "#fff" });
   }
